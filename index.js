4224d5e69efd0db94e18821f74c3724550e183b8
Brad Ayers <brad@bradayers.com>
2019-04-28 05:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,23 +21,9 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   var found = null;
   array.forEach((number, index, array) => {
-    //debugger;
     if (findFn(number)) {
-      //return number;
       found = number;
-      console.log("Got a 'true' here!");
-      console.log(number);
     }
-    console.log(findFn(number));
-    console.log(number);
   })
-
-
-
-  //var temp = findFn();
-  //console.log(temp);
-  //for (var i = 0; i < array.length; i++) {
-  //    console.log(array[i]);
-  //}
   return found;
 }
